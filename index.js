7c01121af5594a174f9bf1e6c0a492663d17689c
brewchetta <chett.tiller@gmail.com>
2024-10-08 15:43:27 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+// GLOBAL CONSTANTS //
+
+
+// CALLBACK FUNCTIONS //
+
+
+// EVENT HANDLERS //
+
+
+// here is an alternate way of attaching event listeners 
+// ( exitWindow would be a predefined function )
+// exitButton.onclick = exitWindow
